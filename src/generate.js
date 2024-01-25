de76beb0f109955b93528caa50a8a3f2e0a0a083
sandraliv <sls40@hi.is>
2024-01-25 12:15:15 +0000

    HTML komid

--- src/generate.js
+++ src/generate.js
@@ -1,10 +1,13 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
 import {
   createDirIfNotExists,
   readFile,
   readFilesFromDir,
 } from './lib/file.js';
-import { parseTeamsJson } from './lib/parse.js';
-
+import { indexTemplate, leikirTemplate, stadaTemplate } from './lib/html.js';
+import { parseGameday } from './lib/parse.js';
+import { calculateStandings } from './lib/score.js';
 const INPUT_DIR = './data';
 const OUTPUT_DIR = './dist';
 
@@ -12,16 +15,42 @@ async function main() {
   await createDirIfNotExists(OUTPUT_DIR);
 
   const files = await readFilesFromDir(INPUT_DIR);
+  //'data' er fylki af parsed gögnum sem við viljum
+  //skrifa niður í HTML skrá sem heitir 'index.html'
+  //og síðan í framhaldinu 'stada.html' og 'leikir.html'
+  const data = [];
   for await (const file of files) {
     if (file.indexOf('gameday') < 0) {
       continue;
     }
-    console.log(file);
     const fileContents = await readFile(file);
+    const parsed = parseGameday(fileContents);
+    data.push(parsed);
   }
-  const teams = await readFile('data/teams.json');
-  const b = parseTeamsJson(teams);
-  console.log(b);
+  //const teams = await readFile('data/teams.json');
+  //const b = parseTeamsJson(teams);
+  //await writeFile(join(OUTPUT_DIR, 'index.html'), indexTemplate(b), {
+  //  flag: 'w+',
+  //});
+
+  //Hnappar á forsíðu á aðrar html síður
+  const Buttons = [
+    { title: 'Staða', html: 'stada.html' },
+    { title: 'Leikir', html: 'leikir.html' },
+  ];
+  const calculatedStandings = calculateStandings(data);
+
+  const indexHtml = indexTemplate('Forsíða', Buttons);
+  const indexFilename = join(OUTPUT_DIR, 'index.html');
+  await writeFile(indexFilename, indexHtml);
+
+  const stadaHtml = stadaTemplate('Staða deildar', calculatedStandings);
+  const stadaFilename = join(OUTPUT_DIR, 'stada.html');
+  await writeFile(stadaFilename, stadaHtml);
+
+  const leikirHtml = leikirTemplate('Leikir', data);
+  const leikirFilename = join(OUTPUT_DIR, 'leikir.html');
+  await writeFile(leikirFilename, leikirHtml);
 }
 
 main().catch((error) => {
--- src/lib/html.js
+++ src/lib/html.js
@@ -1,12 +1,54 @@
-export function indexTemplate(team) {
+export function indexTemplate(title, buttons) {
   const index = `
-      <h1>Kennsluskrá</h1>
-      <h2>Deildir</h2>
+      <h1>Collab-deildin</h1>
+      <h2>Nánari upplýsingar</h2>
       <ul>
-        ${departments
-          .filter((team) => team?.courses?.length > 0)
-          .map((team) => `<li><a href="${team.html}">${team.title}</a></li>\n`)
-          .join('')}
-      </ul>`;
-  return template('Kennsluskrá', index);
+      ${buttons
+        .filter((buttons) => buttons?.title?.length > 0)
+        .map(
+          (buttons) =>
+            `<li><a href="${buttons.html}">${buttons.title}</a></li>\n`,
+        )
+        .join('')}
+      </ul>
+      `;
+  return template(title, index);
+}
+
+export function stadaTemplate(team, data) {
+  const standingsHtml = data.toString();
+  const index = `
+  <h1>Collab-deildin</h1>
+  <section>
+      <h2>Staðan í deildinni</h2>
+      ${standingsHtml};
+      </section>
+      <p><a href="./index.html">Til baka</a></p>
+      `;
+  return template(team, index);
+}
+export function leikirTemplate(team, data) {
+  const gamesHtml = data.toString();
+  const index = `
+  <h1>Collab deildin</h1>
+  <section>
+      <h2>Staða síðustu leikja</h2>
+      ${gamesHtml};
+      </section>
+      <p><a href="./index.html">Til baka</a></p>
+      `;
+  return template(team, index);
+}
+
+export function template(title, content) {
+  return `<!doctype html>
+<html lang="is">
+  <head>
+    <meta charset="utf-8">
+    <title>${title}</title>
+    <link rel="stylesheet" href="./public/styles.css">
+    <script type="module" src="./public/scripts.js"></script>
+  </head>
+  <body>${content}</body>
+</html>`;
 }
