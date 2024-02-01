a3211b301bbc9232c80995e2d4e50ead4b990b71
sandraliv <sls40@hi.is>
2024-02-01 16:17:57 +0000

    profa test aftur

--- src/lib/html.test.js
+++ src/lib/html.test.js
@@ -3,10 +3,9 @@ import { indexTemplate } from './html';
 
 describe('html', () => {
   describe.only('indexTemplate', () => {
-    it('should have a test', () => {
-      expect(indexTemplate()).toBe(
-        '<!doctype html><html lang="is"><head><meta charset="utf-8"><title>undefined</title><link rel="stylesheet" href="./public/styles.css"><script type="module" src="./public/scripts.js"></script></head><body>[object Object]</body></html>',
-      );
+    it('should return template with give title and body', () => {
+      const result = indexTemplate('title', 'body');
+      expect(result).toContain('<title>title<title>');
     });
   });
 });
