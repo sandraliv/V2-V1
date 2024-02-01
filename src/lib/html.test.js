a3f368b914cfa07d206e122ca11f038260afb65a
sandraliv <sls40@hi.is>
2024-02-01 16:14:50 +0000

    profa test

--- src/lib/html.test.js
+++ src/lib/html.test.js
@@ -4,7 +4,9 @@ import { indexTemplate } from './html';
 describe('html', () => {
   describe.only('indexTemplate', () => {
     it('should have a test', () => {
-      expect(indexTemplate()).toBe('html');
+      expect(indexTemplate()).toBe(
+        '<!doctype html><html lang="is"><head><meta charset="utf-8"><title>undefined</title><link rel="stylesheet" href="./public/styles.css"><script type="module" src="./public/scripts.js"></script></head><body>[object Object]</body></html>',
+      );
     });
   });
 });
